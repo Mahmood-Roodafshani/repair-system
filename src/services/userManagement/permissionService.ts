b8d86165b957505634448ddaf285addf6fa42b52
Mahmoodreza <mr.roodafshani@nak-mci.ir>
2025-04-28 11:46:48 +0330

    Bug fix

--- src/services/userManagement/permissionService.ts
+++ src/services/userManagement/permissionService.ts
@@ -1,29 +1,29 @@
-import { axiosInstance } from '../../utils/axios';
+import { apiGet, apiPost, apiPut, apiDelete } from '../baseService';
 import { PermissionDto } from '../../types/responses/userManagement/roleManagement';
 import { ROUTES } from '../../constants/routes';
 
 export const permissionService = {
   async getAll(): Promise<PermissionDto[]> {
-    const response = await axiosInstance.get(ROUTES.USER.ACCESS_CONTROL.FETCH_LIST);
-    return response.data;
+    const response = await apiGet<PermissionDto[]>(ROUTES.USER.ACCESS_CONTROL.FETCH_LIST);
+    return response.content;
   },
 
   async getById(id: string): Promise<PermissionDto> {
-    const response = await axiosInstance.get(`${ROUTES.USER.ACCESS_CONTROL.FETCH_LIST}/${id}`);
-    return response.data;
+    const response = await apiGet<PermissionDto>(`${ROUTES.USER.ACCESS_CONTROL.FETCH_LIST}/${id}`);
+    return response.content;
   },
 
   async create(data: Omit<PermissionDto, 'id'>): Promise<PermissionDto> {
-    const response = await axiosInstance.post(ROUTES.USER.ACCESS_CONTROL.FETCH_LIST, data);
-    return response.data;
+    const response = await apiPost<PermissionDto>(ROUTES.USER.ACCESS_CONTROL.FETCH_LIST, data);
+    return response.content;
   },
 
   async update(id: string, data: Partial<PermissionDto>): Promise<PermissionDto> {
-    const response = await axiosInstance.put(`${ROUTES.USER.ACCESS_CONTROL.FETCH_LIST}/${id}`, data);
-    return response.data;
+    const response = await apiPut<PermissionDto>(`${ROUTES.USER.ACCESS_CONTROL.FETCH_LIST}/${id}`, data);
+    return response.content;
   },
 
   async delete(id: string): Promise<void> {
-    await axiosInstance.delete(`${ROUTES.USER.ACCESS_CONTROL.FETCH_LIST}/${id}`);
+    await apiDelete<void>(`${ROUTES.USER.ACCESS_CONTROL.FETCH_LIST}/${id}`);
   }
 }; 
